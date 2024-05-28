00cc38182e361cf9ffba7b262ee239e22fe5e494
Ali Siavoshi Fard <alsiavos@bess-f1r4s1.clusters.42paris.fr>
2024-05-28 17:12:38 +0200

    Makefile Linked with Mlx ready to use

--- Ali_so_long/so_long.h
+++ Ali_so_long/so_long.h
@@ -6,18 +6,17 @@
 /*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:21:03 by alsiavos          #+#    #+#             */
-/*   Updated: 2024/05/28 15:25:52 by alsiavos         ###   ########.fr       */
+/*   Updated: 2024/05/28 17:07:00 by alsiavos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include ".libft/gnl/get_next_line.h"
 # include ".libft/libft.h"
 # include ".libft/printf/ft_printf.h"
-# include ".libft/gnl/get_next_line.h"
+# include ".minilibx-linux/mlx.h"
 # include <stdio.h>
 # include <unistd.h>
 
--- Ali_so_long/srcs/so_long.c
+++ Ali_so_long/srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:03:16 by alsiavos          #+#    #+#             */
-/*   Updated: 2024/05/28 15:25:31 by alsiavos         ###   ########.fr       */
+/*   Updated: 2024/05/28 16:45:00 by alsiavos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	main(void)
 {
-	printf("yo\n");
-	return (0);
+	void *mlx;
+	void *mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
